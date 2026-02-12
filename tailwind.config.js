2b8fed0755d5028888484bc6a73b470e82543c2f
komphet.me <komphet.me@gmail.com>
2026-02-13 01:59:50 +0700

    Update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -24,8 +24,10 @@ module.exports = {
       },
       letterSpacing: {
         over: "0.28em",
-      }
+      },
     },
   },
   plugins: [],
 };
+
+export default config;
